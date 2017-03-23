faf0d5d0c402aa93718c89b1e1f327c6d1cd30dc
邹正敏 <zouzhengmin@gmail.com>
2017-03-23 19:43:30 +0800

    implement movie#new&create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,49 @@
 class MoviesController < ApplicationController
+
   def index
     @movies = Movie.all
   end
-  
+
+  def new
+    @movie = Movie.new
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path, notice: '已成功更新！'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, alert: '电影已删除。'
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description)
+  end
+
 end
